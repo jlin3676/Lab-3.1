7654a6f085b40e5f548c75873c12c594d96e434d
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2017-11-08 13:16:21 -0500

    we

---
+++ Lab 3.1/src/array/ArrayMethods.java
@@ -0,0 +1,35 @@
+package array;
+
+public class ArrayMethods {
+
+	public static int [] removeDuplicates(int [] list) {
+		
+		int [] copy = copyArray(list);
+		for()
+		return copy;
+	}
+	
+	public static int[][] productArray (int [] arr1, int [] arr2){
+		
+	}
+	
+	public static int [][] pascalTriangle(int n){
+		
+	}
+	
+	public static void printPascalTriangle(int [][] triangle) {
+		
+	}
+	
+	public static int[] copyArray(int[] intArray) {
+		
+		int[] copy = new int[intArray.length];
+		
+		for(int i = 0; i < intArray.length; i++) {
+			copy[i] = intArray[i];
+		}
+		return copy;
+	}
+}
+
+
