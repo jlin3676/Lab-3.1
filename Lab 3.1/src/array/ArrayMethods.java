be07baacd855aa8b816e142aadb1feecb3acf557
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2017-11-09 13:14:19 -0500

    trying sort

--- Lab 3.1/src/array/ArrayMethods.java
+++ Lab 3.1/src/array/ArrayMethods.java
@@ -1,12 +1,25 @@
 package array;
 
 public class ArrayMethods {
-
-	public static int [] removeDuplicates(int [] list) {
+	public static void main(String[] args) {
+		int[] intArray = new int [10];
+		for(int i =0; i < intArray.length; i++) {
+			intArray[i] = (int)(Math.random()*10);
+		}
+		printArray(intArray);
+		System.out.println(removeDuplicates(intArray));
+	}
+	public static int removeDuplicates(int [] list) {
+		int count = 0;
 		
-		int [] copy = copyArray(list);
-		for()
-		return copy;
+		for(int i = 0; i < list.length-1; i++) {
+			for(int j = i+1; j < list.length; j++)
+			if(list[i] == list[j]) {
+				count++;
+			}
+		}
+		int [] copy = new int[list.length-count];
+		return count;
 	}
 	
 	public static int[][] productArray (int [] arr1, int [] arr2){
@@ -30,6 +43,23 @@ public class ArrayMethods {
 		}
 		return copy;
 	}
+	public static void printArray(int[] arr) {
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print("[" + arr[i] + "] ");
+		}
+		System.out.println();
+	}
+	 public void bubbleSort(int [] list)
+	    {
+	        for (int i = 0; i < list.length-1; i++)
+	            for (int j = 0; j < list.length-1-i-1; j++)
+	                if (list[j] > list[j+1])
+	                {
+	                    int temp = list[j];
+	                    list[j] = list[j+1];
+	                    list[j+1] = temp;
+	                }
+	    }
 }
 
 
