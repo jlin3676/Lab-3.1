d13ea0627b236a6936a70ea3de46d2fd8d57bb53
Jacky <jlin3676@bths.edu>
2017-11-13 23:33:20 -0500

    remove duplicates works

--- Lab 3.1/src/array/ArrayMethods.java
+++ Lab 3.1/src/array/ArrayMethods.java
@@ -10,7 +10,7 @@ public class ArrayMethods {
 	
 	
 	public static void main(String[] args) {
-	int [] intArray = {1,2,2,4,2,2,2,5,6,2};
+	int [] intArray = {1,3,2,2,3,1,2,5,7,1,2};
 	int [] intArray2 = {2,4,6,8,10};
 	int [] copy = copyArray(intArray);
 	printArray(removeDuplicates(copy));
@@ -20,34 +20,63 @@ public class ArrayMethods {
 	}
 	
 	
-	public static int[] removeDuplicates(int[] list) {
-		int [] copy = new int[list.length];
-		copy[0] = list[0];
-		for(int i = 1; i < list.length-1; i++) {
-			for(int j = 0; j < copy.length; j++) {
-				if(list[i] != copy[j]) {
-					copy[i] = list[i];
-				}
-				else 
-					i++;
-			}
-		}
-		int count = 0;
-		for(int i = 0; i < copy.length; i++) {
-			if(copy[i] != 0) {
-				count++;
-			}
-		}
-		int[] copyF = new int[list.length-count];
-		for(int i = 0; i < copyF.length; i++) {
-			if(copy[i] != 0) {
-				copyF[i] = copy[i];
-			}
-				
-		}
-		return copyF;
-	}
-	
+	//Removes duplicates from input array
+    public static int[] removeDuplicates(int[] list){
+        int x = 0;
+        int[] intermediateArr = new int[list.length];
+        int[] resultArr;
+        for(int i = 0; i < list.length; i++){
+
+            //Checks if the number at list[i] already exists in intermediateArr
+            if(checkDupes(intermediateArr, list[i])==false){
+
+                //If it doesn't exist, insert it.
+                intermediateArr[x] = list[i];
+                x++;
+            }
+        }
+
+        //Finds the length of the intermediateArr
+        int length = checkLength(intermediateArr);
+
+        //Creates a new array of the same length of the supposed array without dupes
+        resultArr = new int[length];
+
+        //Copes all elements from intermediateArr into resultArr
+        for(int j = 0; j < length; j++){
+
+            resultArr[j] = intermediateArr[j];
+        }
+
+        return resultArr;
+
+    }
+//This method counts the length of an array, stopping at 0
+    public static int checkLength(int[] arr){
+        int count = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i]==0){
+                break;
+            }
+            count++;
+        }
+        return count;
+    }
+
+    //This methods checks if the input number already exists in the array
+    public static boolean checkDupes(int[] arr, int num){
+
+        boolean duplicates = false;
+        for(int i = 0; i < arr.length; i++){
+
+            if(num == arr[i]){
+                duplicates = true;
+                break;
+            }
+        }
+        return duplicates;
+    }
+
 	// multiplies the items at each position 
 	public static int[][] productArray (int [] arr1, int [] arr2){
 		int [][] product = new int[arr1.length][arr2.length];
